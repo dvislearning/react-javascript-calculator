7cd5476e621772ad422278de5309e211a913a644
DV <DVislearning@gmail.com>
2019-05-21 02:58:09 -0700

    write logic to verify entries

--- src/calc-logic.js
+++ src/calc-logic.js
@@ -2,25 +2,69 @@ let total = 0;
 
 let currentValid = null;
 
+const operators = ["+","-","*","/"];
+
 let calcs = [5, "+", 10]
 
-let display = ["0"];
+let display = ["0", "+", "9", "8", ".", "0"];
 
+let firstEntry = display.length === 1 && display[0] === "0";
+
+let afterOperator = operators.includes(display[display.length-1]);
+
+let zeroAfterOperator = operators.includes(display[display.length-2]) && display[display.length-1] === "0";
 
 function processInput(input) {
     if(/[123456789]/.test(input)) {
-        if(display.length === 1 && display[0] === "0") {
+        if(firstEntry) {
             display[0] = input;
         }
-        else {
-            display.push(input);
-        }  
+        else if (zeroAfterOperator) {
+            display.pop()
+        } 
+        display.push(input)
     } else if(input == 0) {
-        if(!display[display.length -1] === 0) {
+        if(validZero()) {
             display.push(input);
-        } else { display.push("not today")  }
+        }
+    } else if (input === ".") {
+        if(validDecimal()) {
+            if(afterOperator) {
+                display.push("0");
+            }
+            display.push(input);
+        }
     }
     return display.join('');
+}
+
+// Checks if number contains more than 1 decimal.
+function validDecimal() {
+    for(let i = display.length -1; i >= 0; i--) {
+        if (display[i] === ".") {
+            return false;
+        }
+        else if(operators.includes(display[i])) {
+            break;
+        }
+    };
+    return true; 
+}
+
+// Checks if number contains leading zeros
+function validZero() {
+    if(firstEntry) {
+        return false;
+    }
+
+    for(let i = display.length -1; i >= 0; i--) {
+        if(operators.includes(display[i])) {
+            if(display[i+1] && display[i+1] == "0" && !(display[i+2])) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 const operations = {
@@ -94,5 +138,6 @@ function evalExpression(string) {
 // console.log(evalExpression("3+3+12*2+2")) //32
 // console.log(evalExpression("3+3+12*2")); //30
 // console.log(calculateExpression(['13','+','41']));
- console.log(processInput("12"));
+console.log(processInput("8"));
 // console.log(display[display.length -1]);
+// console.log(validZero("0"));
